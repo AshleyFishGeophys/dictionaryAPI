e30f1519242e83ee7c589eb91d4a409e158e77a3
Ashley Yaner <ashley.yaner@fractureid.com>
2023-05-03 11:25:44 -0600

    Day 30: Added ability to find definition from csv file given the word and it returns the word and definition from the API

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
 
 
 @app.route("/")
@@ -10,7 +13,8 @@ def home():
 
 @app.route("/api/v1/<word>")
 def about(word):
-    return {"definition": word.upper(),
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    return {"definition": definition,
             "word": word}
 
 
